e043e53de3d3c1a5da7af54e58a87cfedf6a2032
d.krats <danya.krats87@gmail.com>
2024-02-08 15:36:35 +0100

    key

--- src/buisness_logic/chat_gpt_connection.py
+++ src/buisness_logic/chat_gpt_connection.py
@@ -2,13 +2,13 @@ import openai
 
 
 class ChatGptConnection:
-    def __init__(self, api_key:str ="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"):
+    def __init__(self, api_key:str ="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"):
         openai.api_key = api_key
         self.model_engine = "gpt-3.5-turbo-1106"
 
     def send_question(self, question):
     # def send_question_to_ChatGPT(question):
-        openai.api_key = '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'  # Замените 'your_api_key' на ваш API ключ
+        openai.api_key = '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'  # Замените 'your_api_key' на ваш API ключ
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-1106",  # Модель GPT, которую вы хотите использовать
