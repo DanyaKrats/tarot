b93bff0214a67cb5b380abd597faf5e2fef516a2
d.krats <danya.krats87@gmail.com>
2024-02-08 16:17:22 +0100

    base

--- src/api/routes/card_of_the_day_endpoint.py
+++ src/api/routes/card_of_the_day_endpoint.py
@@ -1,13 +1,27 @@
 from fastapi import APIRouter
 from src.buisness_logic.cotd import COTDService
-
+import random
+from src.buisness_logic.card_deck import Deck
 
 cotd_router = APIRouter(prefix="/card-of-the-day")
 
 @cotd_router.get("/{card_numb}")
 def get_cotd(card_numb: int):
+    deck = Deck()
     if card_numb > 77 or card_numb < 0:
         return {"Incorrect Input": card_numb}
     prof_class = COTDService()
-    return {"your_result": prof_class.ask_gpt(card_numb)}
+    return {
+        "Card": deck.get_card_name(card_numb),
+        "your_result": prof_class.ask_gpt(card_numb)
+        }
 
+@cotd_router.get("/")
+def get_cotd():
+    deck = Deck()
+    card_numb = random.randint(1, 77)
+    prof_class = COTDService()
+    return {
+        "Card": deck.get_card_name(card_numb),
+        "your_result": prof_class.ask_gpt(card_numb)
+        }
