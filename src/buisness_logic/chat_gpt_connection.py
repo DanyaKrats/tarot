44e8da75b7204563ac88b416143ebbd947d330eb
d.krats <danya.krats87@gmail.com>
2024-02-08 14:40:59 +0100

    card_of_the_day_eng_works

--- src/buisness_logic/chat_gpt_connection.py
+++ src/buisness_logic/chat_gpt_connection.py
@@ -3,22 +3,27 @@ import httpx
 
 
 class ChatGptConnection:
-    def __init__(self, api_key:str ="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"):
+    def __init__(self, api_key:str ="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"):
         openai.api_key = api_key
-        self.model_engine = "text-davinci-002"
+        self.model_engine = "gpt-3.5-turbo-1106"
 
     def send_question(self, question):
-        # Отправляем вопрос в OpenAI API и получаем ответ от модели
-        prompt = f"Q: {question}\nA:"
-        response = openai.Completion.create(
-            engine=self.model_engine,
-            prompt=prompt,
-            max_tokens=60,
-            n=1,
-            stop=None,
-            temperature=0.7,
+    # def send_question_to_ChatGPT(question):
+        openai.api_key = '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'  # Замените 'your_api_key' на ваш API ключ
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-1106",  # Модель GPT, которую вы хотите использовать
+            messages=[{"role": "user", "content": question}],
+            max_tokens=100,  # Максимальное количество токенов в ответе
+            temperature=0.7,  # Параметр температуры, регулирующий разнообразие ответов
         )
 
-        # Обрабатываем ответ и возвращаем текст
-        answer = response.choices[0].text.strip()
-        return answer
+        if response:
+            return response['choices'][0]['message']['content'].strip()
+        else:
+            return "Error sending request to ChatGPT API"
+
+# # Пример использования
+# question = "What are the symptoms of a cold?"
+# answer = ChatGptConnection().send_question(question)
+# print(answer)
